8dc9e0393b69835c6cb2faf164951c38f45a29e6
Thinh Tran <thinhphuctran@gmail.com>
2024-03-27 23:12:42 +0700

    add the time series layout

--- oop_main.py
+++ oop_main.py
@@ -30,31 +30,41 @@ class BusinessReview():
 
     def fill_info(self, date, seller_origin="ALL", region="ALL", marketplace="ALL", team="ALL"):
         ws = self.main
-        ws.cell(1, 1).value = seller_origin
-        ws.cell(2, 1).value = date
-        ws.cell(3, 1).value = region
-        ws.cell(4, 1).value = marketplace
-        ws.cell(5, 1).value = team
+        ws.cell(1, 1).value = "SELLER ORIGIN"
+        ws.cell(1, 2).value = seller_origin
+        ws.cell(2, 1).value = "DATE"
+        ws.cell(2, 2).value = date
+        ws.cell(3, 1).value = "REGION"
+        ws.cell(3, 2).value = region
+        ws.cell(4, 1).value = "MARKETPLACE"
+        ws.cell(4, 2).value = marketplace
+        ws.cell(5, 1).value = "TEAM"
+        ws.cell(5, 2).value = team
         return ws
 
     def generate_time_series(self, date: datetime.date, trailing: int, time_type: str, row_idx:int =2, col_idx:int =10):
         ws = self.main
         starting_point = max(col_idx, ws.max_column)
-        print(starting_point)
-        for id, col in enumerate(range(starting_point, starting_point+trailing)):
+        for id, col in enumerate(range(starting_point, starting_point+trailing+1)):
+            date_coor = ws.cell(2, 2).coordinate
+            control_coor = ws.cell(1, col).coordinate
 
             if time_type.upper() == "WEEK":
-                ws.cell(1, col).value = id - trailing - 1
-                ws.cell(2, col).value = f"={ws.cell(2, 1).coordinate} + {id}"
+                ws.cell(1, col).value = (id - trailing + 1)*7
+                ws.cell(2, col).value = f"={date_coor} + {control_coor}"
+                ws.cell(3, col).value = f"=_xlfn.ISOWEEKNUM({ws.cell(2, col).coordinate} + 1)"
             elif time_type.upper() == "MONTH":
-                ws.cell(1, col).value = id - trailing - 1
-                ws.cell(2, col).value = f"=EOMONTH({ws.cell(2, 1).coordinate}, {id})"
+                ws.cell(1, col).value = id - trailing
+                ws.cell(2, col).value = f"=EOMONTH({date_coor}, {control_coor})"
+                ws.cell(3, col).value = f"=MONTH({ws.cell(2, col).coordinate})"
             elif time_type.upper() == "QUARTER":
-                ws.cell(1, col).value = id - trailing - 1
-                ws.cell(2, col).value = f"=EOMONTH({ws.cell(2, 1).coordinate}, {id})"
+                ws.cell(1, col).value = id - trailing
+                ws.cell(2, col).value = f"=EOMONTH({date_coor}, {control_coor})"
+                ws.cell(3, col).value = f"=_xlfn.ROUNDUP(MONTH({ws.cell(2, col).coordinate})/3,0)"
             elif time_type.upper() == "YTD":
-                ws.cell(1, col).value = id - trailing - 1
-                ws.cell(2, col).value = f"={ws.cell(2, 1).coordinate} + {id}"
+                ws.cell(1, col).value = id - trailing
+                ws.cell(2, col).value = f"={date_coor} + {control_coor}"
+                ws.cell(3, col).value = f"=_xlfn.ISOWEEKNUM({ws.cell(2, col).coordinate} + 1)"
 
 
 deck = BusinessReview()
@@ -67,7 +77,7 @@ deck.fill_info(date, seller_origin)
 deck.generate_time_series(date, 5, "WEEK")
 deck.generate_time_series(date, 4, "MONTH")
 deck.generate_time_series(date, 2, "QUARTER")
-deck.generate_time_series(date, 1, "YTD")
+deck.generate_time_series(date, 0, "YTD")
 
 wb.save("main.xlsx")
 subprocess.check_call(['open', '-a', 'Microsoft Excel', './main.xlsx'])
